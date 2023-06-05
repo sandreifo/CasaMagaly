38af428e098d93c7f0760cf6a57580240ee3fedc
sandreifo <s.andrei.fuentes@gmail.com>
2023-06-05 15:18:47 -0500

    added functionality to nav button

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const root = document.querySelector(":root");
 const width = window.innerWidth;
 root.style.setProperty("--headerHeight", `${width*612/1280}px`);
+
+document.querySelector("#menuButton").addEventListener("click", function() {
+    console.log("boton")
+
+    document.querySelector(".navigationBarUl").classList.toggle("navigationBarUlResponsive");
+})

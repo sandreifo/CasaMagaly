d7b42027a338b3c1535cc177ac3385a273e47f92
sandreifo <s.andrei.fuentes@gmail.com>
2023-06-07 16:20:32 -0500

    linked form to ensure an email is received

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ root.style.setProperty("--headerHeight", `${width*612/1280}px`);
 document.querySelector("#menuButton").addEventListener("click", function() {
     document.querySelector(".navigationBarUl").classList.toggle("navigationBarUlResponsive");
 })
+
+let formulario = document.getElementById("agendaForm");
+if (formulario != null) {
+formulario.addEventListener('submit', function(e) {
+    alert("Gracias por contactarnos, pronto recibiras nuestra respuesta.");
+})}
+else {}

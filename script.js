c5dcf18d212ed1e9d8818819f9c68c331378e9ee
sandreifo <s.andrei.fuentes@gmail.com>
2023-06-06 20:54:54 -0500

    floating button and finished linking pages

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ const width = window.innerWidth;
 root.style.setProperty("--headerHeight", `${width*612/1280}px`);
 
 document.querySelector("#menuButton").addEventListener("click", function() {
-    console.log("boton")
-
     document.querySelector(".navigationBarUl").classList.toggle("navigationBarUlResponsive");
 })

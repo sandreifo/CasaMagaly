9cac6dea712a81e86a76edf5f5bfe27ba9c4dd2b
sandreifo <s.andrei.fuentes@gmail.com>
2023-06-08 15:53:56 -0500

    gallery slideshow JS

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ formulario.addEventListener('submit', function(e) {
     alert("Gracias por contactarnos, pronto nos comunicaremos contigo.");
 })}
 else {}
+
+let modal = document.getElementById("modalSlide");
+let slideIndex = 1;
+
+function openModal(n){
+    modal.style.display = "block";
+    currentSlide(n)
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+document.addEventListener("click", e => {
+    if (e.target.matches("#modalSlide")){
+        modal.style.display = "none";
+    }
+})
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
